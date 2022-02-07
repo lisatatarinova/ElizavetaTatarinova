a7834427ee344fd9be417483b212108ce745df0e
LISA <elizaveta_tatarinova@epam.com>
2022-02-07 19:32:27 +0300

    Homework 1 commit

---
+++ src/test/java/com/epam/tc/hw1/CalculatorMultiplicationTest.java
@@ -0,0 +1,18 @@
+package com.epam.tc.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CalculatorMultiplicationTest {
+    Calculator calculator = new Calculator();
+
+    @Test(dataProviderClass = DataProviderForCalculator.class,
+            dataProvider = "getTestDataForMultiplicationPositive")
+    public void multiplicationPositiveTest(double a, double b, double expected){
+        double result = calculator.mult(a,b);
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
