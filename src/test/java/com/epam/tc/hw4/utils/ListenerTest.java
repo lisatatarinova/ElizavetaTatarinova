adca5e2096c289c4a12c5931b4d753060fc314be
LISA <elizaveta_tatarinova@epam.com>
2022-02-26 22:36:03 +0300

    Homework 4 completed

--- src/test/java/com/epam/tc/hw4/utils/ListenerTest.java
+++ src/test/java/com/epam/tc/hw4/utils/ListenerTest.java
@@ -1,19 +1,19 @@
 package com.epam.tc.hw4.utils;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-
 @Slf4j
 public class ListenerTest implements ITestListener {
 
+    @Step("Hi Listeners")
     @Override
     public void onTestFailure(ITestResult result) {
-        log.info(result.getName());
         Object driver = result.getTestContext().getAttribute("driver");
         if (driver != null) {
             byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
@@ -21,5 +21,6 @@ public class ListenerTest implements ITestListener {
         } else {
             Allure.addAttachment("Screenshot on failure", "screenshot");
         }
+        result.setEndMillis(System.currentTimeMillis());
     }
 }
